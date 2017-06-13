f77ca883660f588a8398a94c54b7dd26d7e28c27
v.bakin <v.bakin@semrush.com>
2017-06-13 12:45:26 +0300

    docs: add DJANGO_SETTINGS_MODULE to setup django for correctly build docs
    Exception occurred:
      File "/home/docs/checkouts/readthedocs.org/user_builds/django-antispam/envs/latest/lib/python3.5/site-packages/django/conf/__init__.py", line 39, in _setup
        % (desc, ENVIRONMENT_VARIABLE))
    django.core.exceptions.ImproperlyConfigured: Requested settings, but settings are not configured. You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.dj_settings'
 
 
 # -- General configuration ------------------------------------------------
---
+++ tests/dj_settings.py
@@ -0,0 +1,37 @@
+import os
+
+SECRET_KEY = 'secret'
+
+DIRNAME = os.path.dirname(__file__)
+
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase'
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+)
+
+STATIC_URL = '/static/'
+
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
