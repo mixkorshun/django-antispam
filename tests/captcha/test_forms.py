b6600e37e95fad59b4b00f0bb62e1554faa2ddbb
mixkorshun <mixkorshun@gmail.com>
2017-06-14 02:38:28 +0300

    add unit tests for ReCAPTCHA form field

--- tests/captcha/test_forms.py
+++ tests/captcha/test_forms.py
@@ -1,4 +1,9 @@
+import json
 from unittest import TestCase
+from unittest.mock import patch
+
+from django.core.exceptions import ValidationError
+from requests import ConnectionError, Response
 
 from antispam.captcha import widgets
 from antispam.captcha.forms import ReCAPTCHA
@@ -14,20 +19,90 @@ class ReCAPTCHATests(TestCase):
             widget=widgets.ReCAPTCHA,
         )
 
-    def test_field_defaults(self):
-        pass
+    def _get_response(self, json_data):
+        resp = Response()
+        resp.status_code = 200
+        resp._content = json.dumps(json_data).encode('utf-8')
+        return resp
 
-    def test_recaptcha_server_connection_timeout(self):
-        pass
+    @patch('antispam.captcha.forms.settings')
+    def test_field_defaults(self, settings):
+        settings.RECAPTCHA_SITEKEY = 'sitekey-from-settings'
+        settings.RECAPTCHA_SECRETKEY = 'secretkey-from-settings'
+        settings.RECAPTCHA_WIDGET = 'antispam.captcha.widgets.InvisibleReCAPTCHA'
+        settings.RECAPTCHA_TIMEOUT = 15
+        settings.RECAPTCHA_PASS_ON_ERROR = True
 
-    def test_recaptcha_validation_ok(self):
-        pass
+        field = ReCAPTCHA()
 
-    def test_recaptcha_validation_failed(self):
-        pass
+        self.assertEqual('sitekey-from-settings', field.sitekey)
+        self.assertEqual('secretkey-from-settings', field.secretkey)
 
-    def test_recaptcha_validation_unexpected_error(self):
-        pass
+        self.assertIsInstance(field.widget, widgets.InvisibleReCAPTCHA)
+        self.assertEqual(field.sitekey, field.widget.sitekey)
 
-    def test_recaptcha_pass_on_error(self):
-        pass
+        self.assertEqual(15, field.timeout)
+        self.assertEqual(True, field.pass_on_error)
+
+    @patch('antispam.captcha.forms.requests')
+    def test_recaptcha_server_connection_error(self, requests):
+        requests.post.side_effect = ConnectionError()
+
+        with self.assertRaises(ValidationError) as e:
+            self.field.validate('1234')
+
+        self.assertEqual('captcha-error', e.exception.code)
+
+    @patch('antispam.captcha.forms.requests')
+    def test_recaptcha_internal_server_error(self, requests):
+        resp = self._get_response({})
+        resp.status_code = 500
+        requests.post.return_value = resp
+
+        with self.assertRaises(ValidationError) as e:
+            self.field.validate('1234')
+
+        self.assertEqual('captcha-error', e.exception.code)
+
+    @patch('antispam.captcha.forms.requests')
+    def test_recaptcha_validation_ok(self, requests):
+        requests.post.return_value = self._get_response({'success': True})
+
+        self.field.validate('1234')
+        self.assertTrue(True)
+
+    @patch('antispam.captcha.forms.requests')
+    def test_recaptcha_validation_failed(self, requests):
+        requests.post.return_value = self._get_response({'success': False, 'error-codes': ['invalid-input-response']})
+
+        with self.assertRaises(ValidationError) as e:
+            self.field.validate('1234')
+
+        self.assertEqual('captcha-invalid', e.exception.code)
+
+    @patch('antispam.captcha.forms.requests')
+    def test_recaptcha_validation_unexpected_error(self, requests):
+        requests.post.return_value = self._get_response({'success': False, 'error-codes': ['bad-request']})
+
+        with self.assertRaises(ValidationError) as e:
+            self.field.validate('1234')
+
+        self.assertEqual('captcha-error', e.exception.code)
+
+    @patch('antispam.captcha.forms.requests')
+    def test_recaptcha_pass_on_error_request_error(self, requests):
+        self.field.pass_on_error = True
+
+        requests.post.side_effect = ConnectionError()
+
+        self.field.validate('1234')
+        self.assertTrue(True)
+
+    @patch('antispam.captcha.forms.requests')
+    def test_recaptcha_validation_unexpected_error(self, requests):
+        self.field.pass_on_error = True
+
+        requests.post.return_value = self._get_response({'success': False, 'error-codes': ['bad-request']})
+
+        self.field.validate('1234')
+        self.assertTrue(True)
