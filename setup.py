c96adfcf9198f3179b0392ddad08453f587a31cc
v.bakin <v.bakin@semrush.com>
2017-06-14 09:42:33 +0300

    version 0.4.0
    * adds antispam.captcha package with reCAPTCHA integration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-antispam',
-    version='0.3.0',
+    version='0.4.0',
     url='https://github.com/mixkorshun/django-antispam',
     description='Anti-spam protection tools for django applications.',
     keywords=['anti-spam', 'antispam', 'spam'],
